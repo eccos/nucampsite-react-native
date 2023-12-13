d6d805ee8007cda234bd934eca42e7d828f4ecfd
eccos <eccos@users.noreply.github.com>
2023-12-12 22:54:08 -0800

    add send email btn & logic

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,17 @@
+import * as MailComposer from "expo-mail-composer";
 import { ScrollView, Text } from "react-native";
 import * as Animatable from "react-native-animatable";
-import { Card } from "react-native-elements";
+import { Button, Card, Icon } from "react-native-elements";
 
 const ContactScreen = () => {
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      recipients: ["campsites@nucamp.co"],
+      subject: "Inquiry",
+      body: "To whom it may concern:",
+    });
+  };
+
   return (
     <ScrollView>
       <Animatable.View animation="fadeInDown" duration={2000} delay={500}>
@@ -14,6 +23,19 @@ const ContactScreen = () => {
           <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
           <Text>Phone: 1-206-555-1234</Text>
           <Text>Email: campsites@nucamp.co</Text>
+          <Button
+            title={"Send Email"}
+            buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
+            icon={
+              <Icon
+                name="envelope-o"
+                type="font-awesome"
+                color={"#FFF"}
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            onPress={sendMail}
+          />
         </Card>
       </Animatable.View>
     </ScrollView>
